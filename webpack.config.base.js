23c533272da90fbb0f07e8483aa4b2d66caa5991
Pedro <pedro.capriles0@walmart.com>
2023-08-04 10:17:35 -0400

    Add configuration build for production

---
+++ webpack.config.base.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets", "images"),
+          to: "images"
+        }
+      ]
+    })
+  ]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,8 @@
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require("path");
 const { merge } = require("webpack-merge");
 const webpackBaseConfig = require('./webpack.config.base');
 
 module.exports = merge(webpackBaseConfig, {
-  mode: "development",
-  devServer: {
-    static: [
-      {
-        directory: path.join(__dirname, "assets", "images"),
-        publicPath: '/images',
-      },
-    ],
-  },
+  mode: "production"
 });
